124bc4982def49c78488cc95d1c98cfa077b11a7
Kabir Bolatito <kabirbolatito@Talents-MBP.home>
2018-11-04 13:30:56 -0500

    added travis file

---
+++ build/precache-manifest.871f98843c9c5ea4a3af47c61e185b38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0611827f4d76c979350e",
+    "url": "/static/css/main.87c45989.chunk.css"
+  },
+  {
+    "revision": "0611827f4d76c979350e",
+    "url": "/static/js/main.0611827f.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0d24b79b9f7d277007129a5ba7e885af",
+    "url": "/index.html"
+  }
+];
